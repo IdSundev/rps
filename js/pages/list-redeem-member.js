34e985dc4e8d63d68664307bd92b76e9f27968ed
Cef Syarif <chefdeveloper29@gmail.com>
2023-11-20 03:42:04 +0700

    Assets

---
+++ js/pages/list-redeem-member.js
@@ -0,0 +1,23 @@
+  $(function () {
+    "use strict";   
+
+		// Slim scrolling
+  
+	  $('.voucher-image').slimScroll({
+		height: '120px'
+	  });
+		
+	
+  }); // End of use strict
+
+  $(function () {
+    "use strict";   
+
+		// Slim scrolling
+  
+	  $('.item-image').slimScroll({
+		height: '200px'
+	  });
+		
+	
+  }); // End of use strict
---
+++ js/pages/list.js
@@ -0,0 +1,39 @@
+//[Javascript]
+
+
+
+$(function () {
+    "use strict";   
+
+		// Slim scrolling
+  
+	  $('.inner-user-div').slimScroll({
+		height: '342px'
+	  });
+		
+	
+  }); // End of use strict
+
+  $(function () {
+    "use strict";   
+
+		// Slim scrolling
+  
+	  $('.voucher-image').slimScroll({
+		height: '120px'
+	  });
+		
+	
+  }); // End of use strict
+
+  $(function () {
+    "use strict";   
+
+		// Slim scrolling
+  
+	  $('.redeem-voucher-notifications').slimScroll({
+		height: '400px'
+	  });
+		
+	
+  }); // End of use strict
